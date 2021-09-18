b2bb819c9e5d9556ab866bfdd3f2dad9b78cde69
AncaIoana <anca.constantin1@gmail.com>
2021-09-18 13:05:14 +0100

    project to calculate hourly pay

---
+++ hourly_pay.py
@@ -0,0 +1,4 @@
+hours = input ('How many hours did you work today?')
+rate = input ('How much do you get paid per hour? (in $)')
+pay = float(hours) * float(rate)
+print ('your payment for the day is',pay)
